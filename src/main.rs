802d117d621df3dfe1708a27cb017e1d6f67712b
zeljic <zeljic@gmail.com>
2018-02-11 16:32:38 +0100

    Initial state of arguments added. Code rustfmt formatted

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::path::Path;
 
 use clap::{App, Arg};
 
+#[inline]
 fn read_dictionary(dictionary: &str, length: usize) -> Vec<String> {
     match OpenOptions::new().read(true).open(Path::new(dictionary)) {
         Ok(source) => BufReader::new(source)
@@ -32,24 +33,25 @@ fn read_dictionary(dictionary: &str, length: usize) -> Vec<String> {
     }
 }
 
+#[inline]
 fn exec_pwgen(length: usize, size: usize) -> Vec<String> {
     match Command::new("pwgen")
         .args(&["-A", "-0", &length.to_string(), &size.to_string()])
         .output()
-        {
-            Ok(result) => String::from_utf8_lossy(&result.stdout)
-                .split_whitespace()
-                .into_iter()
-                .map(String::from)
-                .collect::<Vec<String>>(),
+    {
+        Ok(result) => String::from_utf8_lossy(&result.stdout)
+            .split_whitespace()
+            .into_iter()
+            .map(String::from)
+            .collect::<Vec<String>>(),
 
-            Err(e) => {
-                println!("Unable to generate pwgen data");
-                eprintln!("ERROR: {}", e);
+        Err(e) => {
+            println!("Unable to generate pwgen data");
+            eprintln!("ERROR: {}", e);
 
-                vec![]
-            }
+            vec![]
         }
+    }
 }
 
 fn main() {
@@ -57,37 +59,40 @@ fn main() {
         .short("c")
         .long("characters")
         .takes_value(true)
-        .required(true);
+        .required(true)
+        .help("");
 
     let arg_words: Arg = Arg::with_name("words")
         .short("w")
         .long("words")
         .takes_value(true)
-        .required(true);
+        .required(true)
+        .help("");
 
     let arg_gen: Arg = Arg::with_name("generate")
         .short("g")
         .long("generate")
         .takes_value(true)
-        .required(true);
+        .required(true)
+        .help("");
 
     let args = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .arg(arg_characters)
-        .arg(arg_words)
-        .arg(arg_gen)
+        .arg(&arg_characters)
+        .arg(&arg_words)
+        .arg(&arg_gen)
         .get_matches();
 
-    let size: usize = value_t_or_exit!(args.value_of("characters"), usize);
-    let gen: usize = value_t_or_exit!(args.value_of("generate"), usize);
-    let words_path = args.value_of("words").unwrap();
+    let characters: usize = value_t_or_exit!(args.value_of("characters"), usize);
+    let generate: usize = value_t_or_exit!(args.value_of("generate"), usize);
+    let words_path: &str = args.value_of("words").unwrap();
 
-    let words = read_dictionary(words_path, size);
+    let words = read_dictionary(words_path, characters);
 
-    exec_pwgen(size, gen)
+    exec_pwgen(characters, generate)
         .into_iter()
-        .filter(|gen| words.contains(gen))
-        .for_each(|item| println!("{}", item));
+        .filter(|ref generated_item| words.contains(generated_item))
+        .for_each(|ref found_item| println!("{}", found_item));
 }
